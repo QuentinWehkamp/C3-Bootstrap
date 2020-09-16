991d254768cc1ed56db6721057108b5474df8d50
QuentinWehkamp <quentinwehkamp@gmail.com>
2020-09-16 11:34:57 +0200

    form changes

--- EindOpdracht/form.php
+++ EindOpdracht/form.php
@@ -42,27 +42,37 @@
         </div>
     </nav>
 
-    <!-- Name and adress form -->
+    <!-- Naam en adres form -->
     <br><br><br><br><br>
     <div class="jumbotron ml-5 mr-5">
         <form>
             <div class="form-row">
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Naam">
+                    <input type="text" class="form-control name">
                 </div>
                 <div class="col">
-                    <input type="text" class="form-control" placeholder="Adres">
+                    <input type="text" class="form-control adres">
                 </div>
-                <div class="col w-50">
+                <div class="col">
                     <input type="submit" class="form-control">
                 </div>
             </div>
         </form>
-    </div>
+        <div class="collapse name-collapse" id="collapseNaam">
+            <div class="card card-body">
+                Vul hier uw naam in.
+            </div>
+        </div>
+        <div class="collapse adres-collapse" id="collapseAdres">
+            <div class="card card-body">
+                Vul hier uw adres in.
+            </div>
+        </div>
 
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+        <script src="index.js"></script>
 </body>
 
 </html>
---
+++ EindOpdracht/index.js
@@ -0,0 +1,13 @@
+$(".name").mouseenter(function () {
+  $(".name-collapse").fadeIn("fast");
+});
+$(".name").mouseleave(function () {
+  $(".name-collapse").fadeOut("fast");
+});
+
+$(".adres").mouseenter(function () {
+    $(".adres-collapse").fadeIn("fast");
+  });
+  $(".adres").mouseleave(function () {
+    $(".adres-collapse").fadeOut("fast");
+  });
